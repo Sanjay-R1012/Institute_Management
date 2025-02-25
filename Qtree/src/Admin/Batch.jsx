b332bab3360b3e48adec9f466dd1a8d85dfc8940
Sanjay-R1012 <sanjayr64748@gmail.com>
2025-02-25 10:18:09 +0530

    Update Batch.jsx

--- Qtree/src/Admin/Batch.jsx
+++ Qtree/src/Admin/Batch.jsx
@@ -86,6 +86,8 @@ const Batch = () => {
           axios.patch(`http://127.0.0.1:3000/batch/update/${id}/`, finished_batch)
           .then((response) => console.log(response))
           .catch((error) => console.log(error));
+
+          unfinishedbatch()
       }
 
       const selectedStudentIds = batch.selectedstudents;
